1374cb630d118266c7980d9b1375ad448ef0e9f2
amanu212 <amanalex66@gmail.com>
2026-03-12 09:02:45 +0300

    Migrated all the html and css codes into React.

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,18 @@ import React from 'react'
 import { Routes, Route } from 'react-router'
 import  HomePage  from './pages/HomePage'
 import CheckoutPage from './Pages/CheckoutPage'
+import OrderPage from './Pages/OrderPage'
+import TrackingPage from './pages/TrackingPage'
 //import CheckoutPage from './pages/CheckoutPage'
 
 
 function App() {
   return (
     <Routes>
-      <Route index element = {<HomePage />} />
+      <Route path = '/' element = {<HomePage />} />
       <Route path="checkout" element = {<CheckoutPage/>} />
+      <Route path="orders" element = {<OrderPage/>} />
+      <Route path="tracking" element = {<TrackingPage/>} />
     </Routes>
   )
 }
