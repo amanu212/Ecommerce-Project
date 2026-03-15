c16e4157d8e93ced4b11fa7b88156425c4f54c5b
amanu212 <amanalex66@gmail.com>
2026-03-15 13:18:45 +0300

    Fetch data from the backend

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -8,6 +8,12 @@ import './HomePage.css'
 
 function HomePage()  {
 
+  fetch('http://localhost:3000/api/products')
+    .then((response) => {
+      response.json().then((data) => {
+        console.log(data)
+      })
+    })
   return (
     <>
       <title>Home Page</title>
